88f4e09a4c9db7d2e4914087ec1d9e24b5438e53
Jan <jan810693@gmail.com>
2024-08-19 14:34:44 +0530

    at specified postion

--- .vscode/Insertion.cpp
+++ .vscode/Insertion.cpp
@@ -1,6 +1,7 @@
-// Insertion : At the Beginning
 #include <iostream>
 using namespace std;
+
+// Insertion : At the Beginning
 // int main(){
 //     int array[10] = {1, 2, 3, 4, 5};
 //     int newElement = 9;
@@ -16,15 +17,35 @@ using namespace std;
 
 
 // Insertion : At the End
+// int main(){
+//     int array[10] = {1, 2, 3, 4, 5};
+//     int newElement = 9;
+    
+//     int size = 0;
+//     while (size < 10 && array[size] != 0){
+//         size++;
+//     }
+//     array[size] = newElement;
+//     for(int i = 0; i < 6; i++){
+//         cout << array[i] << " ";
+//     }
+//     return 0;
+// }
+
+// Insertion : At the Specified Position
 int main(){
     int array[10] = {1, 2, 3, 4, 5};
     int newElement = 9;
+    int insertPosition = 2;
     
-    int size = 0;
-    while (size < 10 && array[size] != 0){
-        size++;
+    int size = 5;
+
+    for (int i = size; i > insertPosition; i--){
+        array[i] = array[i - 1];
     }
-    array[size] = newElement;
+    array[insertPosition] = newElement;
+    size++;
+    
     for(int i = 0; i < 6; i++){
         cout << array[i] << " ";
     }
