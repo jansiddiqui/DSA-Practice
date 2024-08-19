876e9e53936f54248d6ae2021587ebf098e8c81a
Jan <jan810693@gmail.com>
2024-08-19 14:21:50 +0530

    Traversal

---
+++ .vscode/Traversal.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int array[10] = {1, 2, 3, 4, 5};
+    int newElement = 9;
+    for(int i = 6; i > 1; i--){
+        array[i] = array[i - 2];
+    }
+    array[0] = newElement;
+    for(int i = 0; i < 6; i++){
+        cout << array[i] << " ";
+    }
+    return 0;
+}
