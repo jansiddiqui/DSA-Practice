670696642a05aa575aac3f9dcaa3a3f640e9aae9
Jan <jan810693@gmail.com>
2024-08-19 16:19:43 +0530

    Deletion : at the end + at the specified position

--- .vscode/Deletion.cpp
+++ .vscode/Deletion.cpp
@@ -17,3 +17,36 @@ using namespace std;
 //     }
 //     return 0;
 // }
+
+// Deletion : At the End 
+// int main(){
+//     int array[10] = {1, 2, 3, 4, 5};
+//     int size = 5;
+
+//     size--;
+
+//     for (int i = 0; i < size; i++){
+//         cout << array[i] << " ";
+//     }
+
+//     return 0;
+// }
+
+// Deletion : At the Specified position
+int main(){
+    int array[10] = {1, 2, 3, 4, 5};
+    int  size = 5;
+    int deletePosition = 2;
+
+    for (int i = deletePosition; i < size - 1; i++){
+        array[i] = array[i + 1];
+    }
+
+    size--;
+
+    for (int i = 0; i < size; i++){
+        cout << array[i] << " ";
+    }
+
+    return 0;
+}
