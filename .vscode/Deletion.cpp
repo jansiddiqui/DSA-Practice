ab25770f6164fea2cad903fd8cd7c78d54e5ee40
Jan <jan810693@gmail.com>
2024-08-19 16:13:31 +0530

    Deletion : at the beginng

---
+++ .vscode/Deletion.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+// Deletion : At the Beginning
+// int main(){
+//     int array[10] = {1, 2, 3, 4, 5};
+//     int size = 5;
+
+//     for (int i = 0; i < size -1; i++){
+//         array[i] = array[i + 1];
+//     }
+
+//     size--;
+
+//     for (int i = 0; i < size; i++){
+//         cout << array[i] << " ";
+//     }
+//     return 0;
+// }
