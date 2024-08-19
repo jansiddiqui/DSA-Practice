1626bc42076b31909d2c0ae309a1e609afa26071
Jan <jan810693@gmail.com>
2024-08-19 14:30:00 +0530

    at the end

--- .vscode/Insertion.cpp
+++ .vscode/Insertion.cpp
@@ -1,13 +1,30 @@
 // Insertion : At the Beginning
 #include <iostream>
 using namespace std;
+// int main(){
+//     int array[10] = {1, 2, 3, 4, 5};
+//     int newElement = 9;
+//     for(int i = 6; i > 1; i--){
+//         array[i] = array[i - 2];
+//     }
+//     array[0] = newElement;
+//     for(int i = 0; i < 6; i++){
+//         cout << array[i] << " ";
+//     }
+//     return 0;
+// }
+
+
+// Insertion : At the End
 int main(){
     int array[10] = {1, 2, 3, 4, 5};
     int newElement = 9;
-    for(int i = 6; i > 1; i--){
-        array[i] = array[i - 2];
+    
+    int size = 0;
+    while (size < 10 && array[size] != 0){
+        size++;
     }
-    array[0] = newElement;
+    array[size] = newElement;
     for(int i = 0; i < 6; i++){
         cout << array[i] << " ";
     }
