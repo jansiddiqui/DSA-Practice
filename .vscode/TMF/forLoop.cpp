f8900ce6080859c416ad9e6ed5fcc222f9473589
Jan <jan810693@gmail.com>
2024-10-28 23:03:26 +0530

    for loop

---
+++ .vscode/TMF/forLoop.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i;
+    for(int i=1; i<=50; i=i+5){
+        cout<<"Jan Mohammad"<<i<<endl;
+    }
+    cout<<i<<endl;
+    return 0;
+}
