d25dff52b19d3259a9bb4b312cce96fb6488eb8e
Jan <jan810693@gmail.com>
2024-11-05 15:14:46 +0530

    finding kth element using countong sort method

---
+++ Love Babbar/kth3.cpp
@@ -0,0 +1,32 @@
+// Using counting sort
+#include<bits/stdc++.h>
+using namespace std;
+
+int kthSmallest(int arr[], int n, int k){
+    int max_element = arr[0];
+    for(int i=1; i<n; i++){
+        if(arr[i]>max_element){
+            max_element = arr[i];
+        }
+    }
+    int freq[max_element + 1] = {0};
+    for(int i = 0;i<n; i++){
+        freq[arr[i]]++;
+    }
+    int count = 0;
+    for(int i=0; i<= max_element; i++){
+        if(freq[i]!=0){
+            count += freq[i];
+            if(count >= k){
+                return i;
+            }
+        }
+    }
+    return -1;
+}
+int main(){
+    int arr[] = {12, 4, 5, 6, 8};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int k=2;
+    cout<<"The "<<k<<"th smallest element is "<<kthSmallest(arr, n, k)<<endl;
+}
