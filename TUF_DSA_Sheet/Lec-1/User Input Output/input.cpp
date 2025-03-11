a34670b9e3c6e8d8656caaddd758bb821121dd5e
Jan <jan810693@gmail.com>
2025-03-11 20:12:52 +0530

    taking input

---
+++ TUF_DSA_Sheet/Lec-1/User Input Output/input.cpp
@@ -0,0 +1,12 @@
+// #include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int x, y;
+    // int y;
+    cin >> x >>y;
+    // cin >> y;
+    cout << "Value of x: "<<x <<"\n"<<"Value of y: "<<y;
+    return 0;
+}
