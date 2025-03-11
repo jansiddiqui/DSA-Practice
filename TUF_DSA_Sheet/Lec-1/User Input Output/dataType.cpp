568bcbf2e365a1b6fdc58840d3f7691c9bdb8db4
Jan <jan810693@gmail.com>
2025-03-11 20:24:36 +0530

    datatypes

---
+++ TUF_DSA_Sheet/Lec-1/User Input Output/dataType.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    float x = 5.5;
+    double y = 5;
+    cout << "Value of y: "<<y<<"\n"<<"Value of x: "<<x<<"\n";
+    string s1;
+    string s2;
+    cin >> s1 >> s2;
+    cout<< s1 << " " << s2;
+}
