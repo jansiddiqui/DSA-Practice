fbd7322ad3514e40c75b1c8ba6a1f891a5fb7508
Jan <jan810693@gmail.com>
2025-03-11 20:39:07 +0530

    adult prediction

---
+++ TUF_DSA_Sheet/Lec-1/User Input Output/p1.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// write  aprogram to check if are adult or not
+//>= 18, yes
+//<18, no
+
+int main(){
+    int age;
+    cin >> age;
+    if(age>=18){
+        cout<<"Yes";
+    }else{
+        cout<<"No";
+    }
+    return 0;
+}
