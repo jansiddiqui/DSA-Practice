b2a02bf01c16ffedf84bb42ff93f97ba6db9b389
Jan <jan810693@gmail.com>
2025-04-13 16:41:29 +0530

    number check

---
+++ Shraddha Mam DSA/Lec1/NumberCheck.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+
+    if(n>=0){
+        cout<<"Number is positive.";
+    }else{
+        cout<<"Number is Negative.";
+    }
+    return 0;
+}
