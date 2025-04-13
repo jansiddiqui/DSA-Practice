30d056da74d2a1807e39744d0a661bad220325d1
Jan <jan810693@gmail.com>
2025-04-13 17:11:42 +0530

    while loop

---
+++ Shraddha Mam DSA/Lec2/Count.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int count = 1;
+    while(count<=n){
+        cout<<count<<" ";
+        count++;
+    }
+    return 0;
+}
